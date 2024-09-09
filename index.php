ab74f801e9d81b2c89244c4579d9d133510241fc
ne3l9 <pratyush0mishra77@gmail.com>
2024-09-09 07:58:25 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,322 @@
+<?php
+session_start();
+include("connect.php");
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="style_home.css">
+    <title>Virtual Career Counselling</title>
+</head>
+
+<body>
+    <nav>
+        <div class="left">
+            <div class="logo">
+                <img src="saphalata (1).png">
+            </div>
+            <div class="links">
+                <a href="#">Home</a>
+                <a href="#">Institutes</a>
+                <a href="#">Our Counsellors</a>
+                <a href="#">About Us</a>
+                <a href="#">Contact Us</a>
+            </div>
+        </div>
+
+        <div class="buttons">
+            <a href="login.php">Register<i class=''></i></a>
+            <a href="DASH.html"><i class='bx bx-user'></i></a>
+        </div>
+
+    </nav>
+
+    <header>
+        <div class="info">
+            <h1>Your Career Choice Is Our Responsibity</h1>
+            <p>Book A One-One Session With Counsellors And Mentors!</p>
+        </div>
+        <div class="buttons">
+            <button class="see-all">Book An Appointment</button>
+            <button>Search For Your Institue </button>
+        </div>
+        <div class="search">
+            <input type="text" placeholder="Search what you want to be...">
+            <button><i class='bx bx-search'></i></button>
+        </div>
+    </header>
+
+    <div class="content">
+        <div class="separator">
+            <h2>Collaborated Institutes</h2>
+            <a href="#">See all <i class='bx bx-chevron-right'></i></a>
+        </div>
+
+        <div class="courses">
+            <div class="item">
+                <div class="top">
+                    <img src="https://s.yimg.com/zb/imgv1/0d3e1ab8-f640-373e-8a8a-b6294a8a2cae/t_500x300">
+                    <div class="info">
+                        <a href="#">CV RAMAN GLOBAL UNIVERSITY</a>
+                        <p>More than 25 counsellors are from Here</p>
+                        <p></p>
+                        <p></p>
+                    </div>
+                </div>
+                <div class="bottom">
+                    <div class="price">
+                        <h5>REGISTER AS A STUDENT</h5>
+                        
+                    </div>
+                    <h5 class="tag"><span>REGISTER</span> WITH YOUR ROLL NUMBER</h5>
+                </div>
+            </div>
+            <div class="item">
+                <div class="top">
+                    <img src="https://images.hindustantimes.com/img/2021/04/20/1600x900/Scaler_HT_(1)_1618916862423_1618916878436.jpg">
+                    <div class="info">
+                        <a href="#">SCALER UNIVERSITY</a>
+                        <p>More than 15 mentors are from here </p>
+                    </div>
+                </div>
+                <div class="bottom">
+                    <div class="price">
+                        <h5>REGISTER AS A STUDENT</h5>
+                        
+                    </div>
+                    <div class="discount">
+                        <div class="time">
+                            <p></p>
+                            <h5></h5>
+                        </div>
+                        <h5><span>REGISTER</span> WITH YOUR ROLL NUMBER</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="top">
+                    <img src="https://www.silice.org/wp-content/uploads/KIIT-logo.jpg">
+                    <div class="info">
+                        <a href="#">KIIT UNIVERSITY</a>
+                        <p>More than 25 counsellors are from Here</p>
+                        <p></p>
+                        <p></p>
+                    </div>
+                </div>
+                <div class="bottom">
+                    <div class="price">
+                        <h5>REGISTER AS A STUDENT</h5>
+                        <p></p>
+                    </div>
+                    <h5 class="tag"><span>REGISTER</span> WITH YOUR ROLL NUMBER</h5>
+                </div>
+            </div>
+            <div class="item">
+                <div class="top">
+                    <img src="http://4.bp.blogspot.com/-CKYa5TKGifw/T9rEGyOVNhI/AAAAAAAAIGs/dMArRdvtd1k/s1600/4268994534_6e13b04ffb_b.jpg">
+                    <div class="info">
+                        <a href="#">SRM Institute of Science and Technology </a>
+                        <p>More than 15 mentors are from Here</p>
+                        <p></p>
+                        <p></p>
+                    </div>
+                </div>
+                <div class="bottom">
+                    <div class="price">
+                        <h5>REGISTER AS A STUDENT</h5>
+                        <p></p>
+                    </div>
+                    <div class="discount">
+                        <div class="time">
+                            <p></p>
+                            <h5></h5>
+                        </div>
+                        <h5><span>REGISTER</span> WITH YOUR ROLL NUMBER </h5>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="top">
+                    <img src="https://data.org/wp-content/uploads/2023/05/BITS_Pilani-Logo-975x975.png">
+                    <div class="info">
+                        <a href="#">BITS PILANI</a>
+                        <p>More than 25 counsellors is from Here</p>
+                        <p></p>
+                        <p></p>
+                    </div>
+                </div>
+                <div class="bottom">
+                    <div class="price">
+                        <h5>REGISTER AS A Students</h5>
+                        <p></p>
+                    </div>
+                    <h5 class="tag"><span>REGISTER
+                    </span>WITH YOUR ROLL NUMBER</h5>
+                </div>
+            </div>
+            <div class="item">
+                <div class="top">
+                    <img src="https://www.kindpng.com/picc/m/352-3529750_nit-rourkela-logo-hd-hd-png-download.png">
+                    <div class="info">
+                        <a href="#">NATIONAL INSTITUTE OF TECHNOLOGY ROURLKELA</a>
+                        <p>More than 14 mentora are from Here</p>
+                        <p></p>
+                        <p></p>
+                    </div>
+                </div>
+                <div class="bottom">
+                    <div class="price">
+                        <h5>REGISTER AS A STUDENT</h5>
+                        <p></p>
+                    </div>
+                    <div class="discount">
+                        <div class="time">
+                            <p></p>
+                            <h5></h5>
+                        </div>
+                        <h5><span>REGISTER</span> WITH YOUR ROLL NUMBER</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="separator">
+            <h2>Students Comments</h2>
+        </div>
+
+        <div class="comments">
+            <p>These are our students comments. This will help you choose courses better. Also you can contact us for
+                more info!</p>
+            <div class="right">
+                <div class="item">
+                    <p>That was best course for me.</p>
+                </div>
+                <div class="item">
+                    <p>This courses helped me a lot!</p>
+                </div>
+                <div class="item">
+                    <p>That was best course for me.</p>
+                </div>
+                <div class="item">
+                    <p>This courses helped me a lot!</p>
+                </div>
+                <div class="item">
+                    <p>That was best course for me.</p>
+                </div>
+                <div class="item">
+                    <p>This courses helped me a lot!</p>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="separator">
+            <h2>Weekly Podcasts</h2>
+            <a href="#">See all <i class='bx bx-chevron-right'></i></a>
+        </div>
+
+        <div class="podcasts">
+            <div class="item">
+                <div class="top">
+                    <i class='bx bx-headphone'></i>
+                    <div class="info">
+                        <a href="#">Learn Coding</a>
+                        <p>10 Aug 2024</p>
+                        <p>Listened 100 Times</p>
+                    </div>
+                </div>
+                <div class="bottom">
+                    <div class="duration">
+                        <i class='bx bx-play-circle'></i>
+                        <p>01:02:10</p>
+                    </div>
+                    <h5 class="tag"><span>+210</span> Listeners</h5>
+                </div>
+            </div>
+            <div class="item">
+                <div class="top">
+                    <i class='bx bx-headphone'></i>
+                    <div class="info">
+                        <a href="#">Learn Coding</a>
+                        <p>10 Aug 2024</p>
+                        <p>Listened 100 Times</p>
+                    </div>
+                </div>
+                <div class="bottom">
+                    <div class="duration">
+                        <i class='bx bx-play-circle'></i>
+                        <p>01:02:10</p>
+                    </div>
+                    <h5 class="tag"><span>+210</span> Listeners</h5>
+                </div>
+            </div>
+            <div class="item">
+                <div class="top">
+                    <i class='bx bx-headphone'></i>
+                    <div class="info">
+                        <a href="#">Learn Coding</a>
+                        <p>10 Aug 2024</p>
+                        <p>Listened 100 Times</p>
+                    </div>
+                </div>
+                <div class="bottom">
+                    <div class="duration">
+                        <i class='bx bx-play-circle'></i>
+                        <p>01:02:10</p>
+                    </div>
+                    <h5 class="tag"><span>+210</span> Listeners</h5>
+                </div>
+            </div>
+        </div>
+
+      
+    </div>
+
+    <footer>
+        <div class="columns">
+            <div class="col">
+                <h5>Site Links</h5>
+                <a href="#">Articles</a>
+                <a href="#"></a>
+                <a href="#">About Us</a>
+                <a href="#">Contact Us</a>
+            </div>
+            <div class="col">
+                <h5>Other Pages</h5>
+                <a href="#">404 Page</a>
+                <a href="#">Login|Register</a>
+                <a href="#">Privacy Policy</a>
+            </div>
+            <div class="col">
+                <h5>Collaboration</h5>
+                <a href="#">Work With Us</a>
+                <a href="#">Affiliate</a>
+                <a href="#">Support</a>
+            </div>
+            <div class="col last">
+                <h5>About Us</h5>
+                <p>At Team Mavericks, we believe that career growth isn't just about finding the right job—it's about discovering and unleashing your full potential. Our dedicated team of career counselors is committed to guiding you through every stage of your professional journey with personalized, strategic advice and cutting-edge tools.</p>
+                <div class="social-links">
+                   <a href="https://www.instagram.com/reel/C-QoL0Bh4s4/"> <i class='bx bxl-instagram'></i></a>
+                    <i class='bx bxl-dribbble'></i>
+                    <i class='bx bxl-linkedin'></i>
+                    <i class='bx bxl-twitter'></i>
+                </div>
+            </div>
+        </div>
+        <div class="copyright">
+            <p>Copyright © 2024 Team Mavericks, All Rights Reserved.</p>
+        </div>
+    </footer>
+
+
+</body>
+
+</html>
